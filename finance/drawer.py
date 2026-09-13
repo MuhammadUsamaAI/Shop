47bfb545dd2b93ccdeadb9a8c998676fd24e41fe
MuhammadUsamaAI <musama.qureshi22@gmail.com>
2026-09-13 18:45:18 +0500

    fully functional drawer class

--- finance/drawer.py
+++ finance/drawer.py
@@ -2,8 +2,9 @@ from pydantic import BaseModel, PrivateAttr
 from helper_functions import current_date_time
 from typing import List, Tuple
 
+
 class Drawer(BaseModel):
-    _balance: int = PrivateAttr(default=0)
+    _balance: int = PrivateAttr(default=10000)
     _time: str = PrivateAttr(default=current_date_time())
     _cash_flow_logs: List[Tuple[int, str]] = PrivateAttr(default_factory=list)
 
@@ -23,21 +24,49 @@ class Drawer(BaseModel):
     def balance(self) -> int:
         return self._balance
 
-    @balance.setter
-    def balance(self, value: int | str):
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("Balance cannot be negative")
-            self._balance += value
-            self._cash_flow_logs.append((value, self.time))
-        elif isinstance(value, str):
+    def _parse_and_validate_value(self, value: int | str) -> int:
+        if isinstance(value, str):
             try:
-                converted = int(value)
-                if converted < 0:
-                    raise ValueError("Balance cannot be negative")
-                self._balance += converted
-                self._cash_flow_logs.append((converted, self.time))
+                value = int(value)
             except ValueError:
                 raise ValueError("Value can only be a numeric type")
-        else:
+        elif not isinstance(value, int):
             raise TypeError("Balance must be an int or a numeric string")
+
+        if value < 0:
+            raise ValueError("Transaction amount cannot be negative")
+        return value
+
+    @property
+    def balance_adder(self) -> int:
+        return self._balance
+
+    @balance_adder.setter
+    def balance_adder(self, value: int | str):
+        clean_value = self._parse_and_validate_value(value)
+
+        self._balance += clean_value
+        self._cash_flow_logs.append((clean_value, self.time))
+
+    @property
+    def balance_negator(self) -> int:
+        return self._balance
+
+    @balance_negator.setter
+    def balance_negator(self, value: int | str):
+        clean_value = self._parse_and_validate_value(value)
+
+        if clean_value > self._balance:
+            raise ValueError("Insufficient Funds in Accounts")
+
+        self._balance -= clean_value
+        self._cash_flow_logs.append((-clean_value, self.time))
+
+
+
+d = Drawer()
+d.balance_negator = 650
+d.balance_adder = 900
+d.balance_negator = 1000
+print(f"Current Balance: {d.balance}")
+print(f"Cash Flow Logs: {d.cash_flow_logs}")
--- inventory/inventry_producer_module.py
+++ inventory/inventry_producer_module.py
@@ -87,23 +87,3 @@ def schema_validator_from_db() -> Tuple[str, Optional[str], Optional[str], int]:
     )
     quantity: int = validator_func(quantity_validator, f'Please enter the quantity of the items to be bought: ', cast_to=int)
     return name, brand, type_, quantity
-
-# def inventry_addititor_test() -> bool:
-#     choice: str = input("Do you want to add an item? (y/n): ").strip().lower()
-#
-#     if choice in ['yes', 'y']:
-#         name, brand, type_, price_each, quantity, date_time, total_price = schema_validator()
-#         print("\n📦 Created Object Blueprint:\n", name, brand, type_, price_each, quantity, date_time, total_price)
-#         print('✨ Addition Successful!\n')
-#         time.sleep(1)
-#         return True
-#
-#     elif choice in ['no', 'n']:
-#         print('Good Bye!')
-#         time.sleep(1)
-#         return False
-#
-#     else:
-#         print("Invalid choice. Please enter 'y' or 'n'.")
-#         return True  # Keeps the loop running on invalid input
-# schema_validator_from_db()
