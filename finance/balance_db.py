70c7a6315c25a3d0c87da9c08a5f2ec31187ac99
MuhammadUsamaAI <musama.qureshi22@gmail.com>
2026-09-14 15:28:28 +0500

    added db for finance handeling balance_db

---
+++ finance/balance_db.py
@@ -0,0 +1,103 @@
+from typing import Optional
+from sqlalchemy import Integer, String, create_engine, select
+from sqlalchemy.orm import Mapped, mapped_column, Session, DeclarativeBase
+from helper_functions import current_date_time, main_path, path_corretor
+
+
+abs_path = path_corretor('finance', main_path(), 'shop_database.db')
+print(abs_path)
+
+class Base(DeclarativeBase):
+    pass
+
+class Balance_Sheet(Base):
+    __tablename__ = 'balance_sheet'
+
+    id:Mapped['int'] = mapped_column(primary_key=True)
+    balance:Mapped['int'] = mapped_column(Integer)
+    transection_time:Mapped['str'] = mapped_column(String(50))
+    comment:Mapped[Optional['str']] = mapped_column(String(100))
+
+    def __repr__(self):
+        return f'id = {self.id}, balance = {self.balance},' \
+               f' transection_time = {self.transection_time},' \
+               f' comment = {self.comment}'
+
+
+class FinanceDBHandler:
+    def __init__(self, db_url:str = "sqlite://"):
+        self.engine = create_engine(db_url, echo=False)
+        Base.metadata.create_all(self.engine)
+
+    def last_row(self, session:Session)->Balance_Sheet|None:
+        smt = select(Balance_Sheet).order_by(Balance_Sheet.id.desc()).limit(1)
+        return session.scalars(smt).first()
+
+    def add_to_db(self,
+              amount:str|int,
+              comment:str=None
+              )->None:
+        amount = int(amount)
+        with Session(self.engine) as session:
+            existing_record = self.last_row(session)
+            if existing_record:
+                last_balance = existing_record.balance
+                session.add(Balance_Sheet(
+                    balance = amount + last_balance,
+                    transection_time = current_date_time(),
+                    comment = comment
+                ))
+                session.commit()
+                print(f'balance updated with {amount}, new amount is '
+                      f'{amount+last_balance}', '\n')
+            else:
+                session.add(Balance_Sheet(
+                    balance = amount,
+                    transection_time = current_date_time(),
+                    comment = comment
+                ))
+                session.commit()
+                print(f'new table is made with balance = {amount}', '\n')
+
+    def negate_balance(self,
+                       amount:int,
+                       comment:str = None
+                       ):
+        amount = int(amount)
+        with Session(self.engine) as session:
+            existing_record = self.last_row(session)
+            if existing_record:
+                last_balance = existing_record.balance
+                if amount> last_balance:
+                    raise ValueError('Print Insufficient Funds, '
+                                     f'Available Balance is {last_balance} '
+                                     f'Amount Required is {amount}')
+                else:
+                    session.add(
+                        Balance_Sheet(
+                            balance = last_balance - amount,
+                            transection_time = current_date_time(),
+                            comment = comment
+                        )
+                    )
+                    session.commit()
+                    print(f'remaining balance is {last_balance-amount}')
+            else:
+                print('no table exists')
+
+    def watch_db(self):
+        with Session(self.engine) as session:
+            stmt = select(Balance_Sheet)
+            for val in session.scalars(stmt):
+                print(val)
+
+
+f = FinanceDBHandler()
+f.negate_balance(amount=200)
+f.add_to_db(amount=600)
+f.add_to_db(amount=400)
+f.negate_balance(amount=500)
+f.negate_balance(amount=300)
+print(f.watch_db())
+
+
--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 from zoneinfo import ZoneInfo
-
+from os import PathLike
 time_format: str = '%d-%m-%Y %H:%M:%S'
 
 
@@ -11,3 +11,14 @@ def current_date_time() -> str:
 
 def main_path() -> str:
     return os.getcwd()
+
+
+def path_corretor(
+    key_to_remove: str, path: str | PathLike[str], key_to_add: str
+) -> str:
+    path_str = str(path)
+    if key_to_remove in path_str:
+        path_str = path_str.replace(
+            "finance", ""
+        )
+    return os.path.join(path_str, key_to_add)
