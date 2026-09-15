f96c9e511c8d643a333eb17c393b1131226619ac
MuhammadUsamaAI <musama.qureshi22@gmail.com>
2026-09-15 12:22:27 +0500

    making app manu functionable

--- apis/manu.py
+++ apis/manu.py
@@ -1,5 +1,7 @@
 from user_validation import UserValidation, validation_function
-enter_string = 'please enter your '
+
+enter_your_string = 'please enter your '
+enter_person_string = 'please enter person'
 
 
 def manu()->None:
@@ -8,17 +10,38 @@ def manu()->None:
     acc_type = validation_function(UserValidation.Acc_Type.value, 'Select account type')
     if acc_type.lower() == 'manager':
         print('Welcome to the Managerial Manu', '\n')
-        name = validation_function(UserValidation.Name.value, f'{enter_string} name')
-        age = validation_function(UserValidation.Age.value, f'{enter_string} age ')
-        pin = validation_function(UserValidation.Pin.value, f'{enter_string} pin')
-        print(f'hello {acc_type}:{name}')
+        name = validation_function(UserValidation.Name.value, f'{enter_your_string} name')
+        #age = validation_function(UserValidation.Age.value, f'{enter_your_string} age ')
+        pin = validation_function(UserValidation.Pin.value, f'{enter_your_string} pin')
+
+        print(f'hello {acc_type}:{name}', '\n')
+        choice_account_creation = validation_function(UserValidation.AddRmove.value,
+                                     'do you want to add or remove user account?')
+        if choice_account_creation.lower() == ('add'):
+            print("please enter the person's details below to add", '\n')
+            name = validation_function(UserValidation.Name.value, f'{enter_person_string} name')
+            age = validation_function(UserValidation.Age.value, f'{enter_person_string} age ')
+            pin = validation_function(UserValidation.Pin.value, f'{enter_person_string} pin')
+            print('\n')
+            print(f'Person {name} is added to the database as User')
+        elif choice_account_creation.lower() == ('remove'):
+            print("please enter the person's details below to remove", '\n')
+            name = validation_function(UserValidation.Name.value, f'{enter_person_string} name')
+            #age = validation_function(UserValidation.Age.value, f'{enter_person_string} age ')
+            pin = validation_function(UserValidation.Pin.value, f'{enter_person_string} pin')
+            print('\n')
+            print(f'Person {name} is removed from the database as User')
+
+
+
 
     elif acc_type.lower() == 'user':
         print('Welcom to the User Manu', '\n')
-        name = validation_function(UserValidation.Name.value, f'{enter_string} name')
-        age = validation_function(UserValidation.Age.value, f'{enter_string} age ')
-        pin = validation_function(UserValidation.Pin.value, f'{enter_string} pin')
+        name = validation_function(UserValidation.Name.value, f'{enter_your_string} name')
+        age = validation_function(UserValidation.Age.value, f'{enter_your_string} age ')
+        pin = validation_function(UserValidation.Pin.value, f'{enter_your_string} pin')
         print(f'hi {acc_type}:{name}')
+
     else:
         print('unspecified account type')
         return True
--- apis/user_validation.py
+++ apis/user_validation.py
@@ -15,12 +15,17 @@ user_name = TypeAdapter(
         )
     ]
 )
+
+
 user_literal_types = ('user', 'manager', 'User', 'Manager', 'MANAGER', 'USER')
+user_literal_choices = ('yes', 'no', 'Yes', 'No', 'YES', 'NO')
+user_literal_add_remove = ('add', 'remove', 'ADD', 'REMOVE', 'Add', 'Remove')
 
 user_age:TypeAdapter[int] = TypeAdapter(Annotated[int, Field(ge= 18, le=99)])
 user_type:TypeAdapter[Literal[Tuple]] = TypeAdapter(Literal[user_literal_types])
 user_pin:TypeAdapter[int] = TypeAdapter(Annotated[int, Field(ge=0, lt=9999)])
-user_choice:TypeAdapter[str] = TypeAdapter(Literal['yes', 'no', 'Yes', 'No', 'YES', 'NO'])
+user_choice:TypeAdapter[str] = TypeAdapter(Literal[user_literal_choices])
+user_add_remove:TypeAdapter[str] = TypeAdapter(Literal[user_literal_add_remove])
 
 class UserValidation(enum.Enum):
     Name = user_name
@@ -28,6 +33,7 @@ class UserValidation(enum.Enum):
     Choice = user_choice
     Pin = user_pin
     Age = user_age
+    AddRmove = user_add_remove
 
 def validate_input_type(arg: str|int, validation_type: TypeAdapter):
     if isinstance(arg, str) and (validation_type == user_age or validation_type == user_pin):
